c06339b583480015702ba970087a96dd89c07eca
ewen le letty <ewenlele@localhost.localdomain>
2021-03-08 15:23:18 +0100

    [fix] exo 2

--- part1/missionData.ts
+++ part1/missionData.ts
@@ -1,8 +1,8 @@
 const operationCode : number = 42;
 const operationAllowed : boolean = true;
 const operationName : string = "KSW";
-const agents : {name: string, code: number}[] = [
+const agents : {name?: string, code: number}[] = [
     {"name": "Kreog", "code": 42},
     {"name": "Mate", "code": 21},
-    {"name": "", "code": 0.07}
+    {"code": 0.07}
 ];
