d02e1782feb48902c8d564d76c0c7e639bcfeca2
ewen le letty <ewenlele@localhost.localdomain>
2021-03-08 16:55:24 +0100

    [fix] exo 7

--- part1/missionManager.ts
+++ part1/missionManager.ts
@@ -3,8 +3,15 @@ import { mission } from './mission.ts';
 export async function missionTester(agents : {name?: string, code: number}[]): Promise<void> {
     const result = await mission(agents);
     try {
-            throw(result);
+        if (result == true)
+            throw('success');
+        else if (result == 'Bad agent')
+            throw('Bad agent');
+        else
+            throw(false);
     } catch (err) {
+        if (err = 'Bad agent')
+            console.log('Bad agent');
         console.log('Mission report: ' + err);
     }
 }
