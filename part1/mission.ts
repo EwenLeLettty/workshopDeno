8c9f53b0eb57bee08d1a616aa03da9f1ce743d0c
ewen le letty <ewenlele@localhost.localdomain>
2021-03-08 15:53:16 +0100

    [add] exo 5

---
+++ part1/mission.ts
@@ -0,0 +1,11 @@
+function mission(agents : {name?: string, code: number}[]): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        for (const item of agents) {
+            if (item.code != 42 && item.code != 0.01 && item.code != 0.07)
+                return reject('Bad agent');
+            if (item.code == 0.07)
+                return resolve(true);
+            return resolve(false);
+        }
+    });
+}
