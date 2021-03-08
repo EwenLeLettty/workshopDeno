1b8d8a3ed8753f122896092e5bf233c1f5debdf3
ewen le letty <ewenlele@localhost.localdomain>
2021-03-08 16:14:44 +0100

    [fix] exo 5

--- part1/mission.ts
+++ part1/mission.ts
@@ -1,4 +1,4 @@
-export function mission(agents : {name?: string, code: number}[]): Promise<boolean> {
+export async function mission(agents : {name?: string, code: number}[]): Promise<boolean> {
     return new Promise((resolve, reject) => {
         for (const item of agents) {
             if (item.code != 42 && item.code != 0.01 && item.code != 0.07)
@@ -6,6 +6,6 @@ export function mission(agents : {name?: string, code: number}[]): Promise<boole
             if (item.code == 0.07)
                 return resolve(true);
             return resolve(false);
-        }
+        };
     });
 }
